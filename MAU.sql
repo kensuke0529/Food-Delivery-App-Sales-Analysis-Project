3e70e2ff4e32bd04efc48f1c92441ce1443a03da
kensuke0529 <kenkou1001@gmail.com>
2024-12-05 21:04:26 -0700

    update

---
+++ MAU.sql
@@ -0,0 +1,36 @@
+-- registration by each month
+WITH reg_dates as (
+    SELECT 
+        user_id,
+        MIN(order_date) as reg_date
+    FROM orders
+    GROUP BY user_id
+)
+SELECT
+    DATE_TRUNC('month', reg_date) as month,
+    COunt( user_id) as reg_number
+FROM reg_dates
+GROUP BY month
+ORDER BY month ASC;
+
+--MAU
+WITH mau AS (
+  SELECT
+    DATE_TRUNC('month', order_date) :: DATE AS month,
+    COUNT(DISTINCT user_id) AS mau
+  FROM orders
+  GROUP BY month),
+
+mau_with_lag AS (
+  SELECT
+    month,
+    mau,
+    GREATEST(LAG(mau) OVER (ORDER BY month ASC),1) AS last_mau
+  FROM mau)
+
+SELECT
+  -- Calculate the MoM MAU growth rates
+  month,
+  ROUND((mau - last_mau):: numeric/ last_mau ,2) AS growth_rate
+FROM mau_with_lag
+ORDER BY month;
---
+++ retention.sql
@@ -0,0 +1,19 @@
+-- retention rate
+WITH user_monthly_activity AS (
+  SELECT DISTINCT
+    DATE_TRUNC('month', order_date) :: DATE AS month,
+    user_id
+  FROM orders)
+
+SELECT
+  previous.month,
+  ROUND(
+    COUNT(distinct current.user_id) :: numeric /
+    GREATEST(COUNT(DISTINCT previous.user_id), 1), -- avoid dividing by 0 
+  2) AS retention_rate
+FROM user_monthly_activity AS previous
+LEFT JOIN user_monthly_activity AS current
+ON current.user_id = previous.user_id
+AND previous.month = (current.month -INTERVAL'1 month')
+GROUP BY previous.month
+ORDER BY previous.month ASC;
